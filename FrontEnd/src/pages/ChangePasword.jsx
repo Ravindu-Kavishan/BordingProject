de3412b0a0c3e8b001c981a818e44bbf07f286c1
Ravindu-Kavishan <Priyadarshandmrk.22@uom.lk>
2025-05-17 17:37:31 +0530

    changed pwd  page added

--- FrontEnd/src/pages/ChangePasword.jsx
+++ FrontEnd/src/pages/ChangePasword.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import InputField from "../components/InputField";
+import ErrorAlert from "../components/ErrorAllert";
+import SuccessMSG from "../components/SuccessMSG";
+import Button from "../components/Button";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faEnvelope,
+  faPhone,
+  faCommentDots,
+  faLock,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function ChangePasword() {
+  const [formData, setFormData] = useState({
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errorMsg, setErrorMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    const {  password, confirmPassword } =
+      formData;
+
+    if (
+      !password ||
+      !confirmPassword
+    ) {
+      setErrorMsg("Please fill in all fields.");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setErrorMsg("Passwords do not match.");
+      return;
+    }
+
+    console.log("Registration Data:", {
+      password,
+    });
+
+    setSuccessMsg("Registered successfully!");
+    setFormData({
+      password: "",
+      confirmPassword: "",
+    });
+    navigate("/Login")
+  };
+
   return (
-    <div>
-      
+    <div className="min-h-screen flex items-center justify-center auth-bg">
+      <div className="w-full max-w-md p-6 rounded-2xl shadow-2xl  auth-secondry-bg">
+        <h2 className="text-center text-2xl font-extrabold mb-6 auth-theam-text-color">
+          Reset Pasword
+        </h2>
+
+        {errorMsg && <ErrorAlert message={errorMsg} />}
+        {successMsg && <SuccessMSG message={successMsg} />}
+
+        <InputField
+          placeholder="Password"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+          name="password"
+          isPassword={true}
+          icon={<FontAwesomeIcon icon={faLock} />}
+        />
+
+        <InputField
+          placeholder="Confirm Password"
+          value={formData.confirmPassword}
+          onChange={(e) =>
+            setFormData({ ...formData, confirmPassword: e.target.value })
+          }
+          name="confirmPassword"
+          isPassword={true}
+          icon={<FontAwesomeIcon icon={faLock} />}
+        />
+
+        <Button
+          text="Submit"
+          color="auth-button"
+          tcolor="auth-button-text"
+          onClick={handleSubmit}
+        />
+        <Button
+          text="<-Back"
+          color="auth-button-secondry"
+          tcolor="auth-button-secondry-text"
+          onClick={() => navigate("/SubmitOTP")}
+        />
+      </div>
     </div>
-  )
+  );
 }
